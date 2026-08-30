ef3295c9e51530bf850093c392a2dbf2bd3527cf
Fernando Carvalho <fernando.bernardini25@gmail.com>
2026-08-30 19:14:54 -0300

    Estrutura inicial projeto java

---
+++ src/test/java/br/com/tcc/cyber/CyberApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcc.cyber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
